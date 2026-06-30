09c999d1946871dc718b1f46c7f21c6261ca5806
AlexandrHlebnov <alexandrhlebnov@gmail.com>
2026-06-30 09:19:19 +0300

    Initial project structure

---
+++ .gitignore.py
@@ -0,0 +1,19 @@
+# Virtual environment
+.venv/
+
+# PyCharm
+.idea/
+
+# Python cache
+__pycache__/
+*.pyc
+
+# Test reports
+allure-results/
+allure-report/
+
+# Logs
+*.log
+
+# Environment variables
+.env
